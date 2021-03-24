3f80a5c2ae4c3f9e19f06fb9fc0f88e172771c13
Tanushree Shaw <63643049+TanushreeShaw@users.noreply.github.com>
2021-03-24 21:41:39 +0530

    Update QueryUtils.java

--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -2,7 +2,6 @@ package com.example.android.quakereport;
 
 import android.text.TextUtils;
 import android.util.Log;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
